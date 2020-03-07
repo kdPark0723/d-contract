7ac518b3ff15d27fc6cfdbc15029e1cffc592af3
박시율 <kdpark0723@gmail.com>
2020-03-07 17:13:06 +0900

    refactoring

--- lib/decorator/assert.ts
+++ lib/decorator/assert.ts
@@ -1,3 +1,10 @@
-import required = require('./required');
+import createParameterDecoratorFunction = require('../parameter/create-parameter-decorator-function');
+import assert = require('assert');
 
-export = { required };
+export = function (callback: (param, info) => boolean, message: string) {
+  return createParameterDecoratorFunction(Symbol(), ((param, info) => {
+    const value = callback(param, info);
+    const localMessage = `[${info.target.constructor.name || JSON.stringify(info.target)}.${info.key.toString()}:${info.index}] ${param}, ${message}`;
+    return assert(value, localMessage);
+  }))
+};
--- lib/decorator/required.ts
+++ lib/decorator/required.ts
@@ -1,7 +1,3 @@
-import createParameterDecoratorFunction = require('../parameter/create-parameter-decorator-function');
-import assert = require('assert');
+import assert = require('./assert');
 
-export = createParameterDecoratorFunction(Symbol('required'), ((param, info) => {
-  const message = `[${JSON.stringify(info.target)}:${info.key.toString()}:${info.index}] ${param} !== undefined`;
-  return assert(param !== undefined, message);
-}));
+export = assert((param) => param !== undefined, 'Parameter Required.');
